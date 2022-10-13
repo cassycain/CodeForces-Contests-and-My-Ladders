97dd4fe3517651c9acdac7f269aacbeb00de8e5f
Cass <ganeshdidugu6@gmail.com>
2022-10-13 23:16:50 +0530

    upsolved 1742D

--- 1742/1742D.cpp
+++ 1742/1742D.cpp
@@ -9,44 +9,32 @@
 
 using namespace std;
 
-vector<vector<int>>factors(1001);
-vector<int>p;
-unordered_map<int,int>pid;
-
 void init(){
-    for(int i=2;i<=1000;i++){
-        if(factors[i].size() == 0){
-            pid[i] = p.size();
-            p.push_back(i);
-            factors[i].push_back(i);
-            for(int j=i;j<=1000;j+=i){
-                factors[j].push_back(i);
-            }
-        }
-    }
+    
 }
 
 int gcd(int a, int b){
-    if(a == 0) return b;
-    else return gcd(b%a, a);
+    return a ? gcd(b%a, a) : b;
 }
 
 void solve(){
     int n; cin>>n;
-    vector<int>a(n);
-    
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    unordered_map<int, int>pos;
+    for(int i=1;i<=n;i++){
+        int x; cin>>x;
+        pos[x] = i;
     }
-    
-    int m = p.size();
-    vector<vector<int>>memo(n, vector<int>(m, true));
-    for(int i=0;i<n;i++){
-        for(int pf:factors[a[i]]){
-            
+
+    int ans = -1;
+    for(int i=1;i<=1000;i++){
+        for(int j=i;j<=1000;j++){
+            if(pos.find(i) != pos.end() && pos.find(j) != pos.end() && gcd(i, j) == 1){
+                ans = max(ans, pos[i] + pos[j]);
+            }
         }
     }
 
+    cout<<ans<<"\n";
 }
 
 
